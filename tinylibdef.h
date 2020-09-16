f9242cc8a277f1b48fc058916ba0b3025ff375c5
charlesgervais <charles.gervais.dumont@gmail.com>
2020-09-16 17:26:41 -0400

    Fix Global Interrupt Macros

--- tinylibdef.h
+++ tinylibdef.h
@@ -18,8 +18,7 @@ typedef unsigned char UBaseType_t;
 #define pdFALSE (0)
 #define pdTRUE (1)
 
-#define DISABLE_INTERRUPTS()	asm volatile ( "cli" :: );
-#define ENABLE_INTERRUPTS()		asm volatile ( "sei" :: );
-
+#define DISABLE_INTERRUPTS()	__asm__ __volatile__ ( "cli" ::: "memory");
+#define ENABLE_INTERRUPTS()		__asm__ __volatile__ ( "sei" ::: "memory");
 
 #endif /* TINYLIBDEF_H_ */

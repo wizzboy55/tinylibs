622472a2025e44c046d9278fb34bee8c7e89886c
charlesgervais <charles.gervais.dumont@gmail.com>
2020-10-16 14:56:45 -0400

    GPIO: Add TOGGLE macro and remove old macros

--- gpio/gpio.h
+++ gpio/gpio.h
@@ -18,27 +18,12 @@ typedef enum {ePortA, ePortB, ePortC} ePort_t;
 #define GPIO_PORTB	1
 #define GPIO_PORTC	2
 
-#ifdef PORTA
-#define GPIO_PORTA_SETPIN(pin)	PORTA.OUTSET = (1 << pin)
-#define GPIO_PORTA_CLRPIN(pin)	PORTA.OUTCLR = (1 << pin)
-#define GPIO_PORTA_SETOUT(pin)	PORTA.DIRSET = (1 << pin)
-#define GPIO_PORTA_SETIN(pin)	PORTA.DIRCLR = (1 << pin)
-#define GPIO_PORTA_READPIN(pin)	((PORTA.IN & (1 << pin)) != 0)
-#endif
-
-#ifdef PORTB
-#define GPIO_PORTB_SETPIN(pin)	PORTB.OUTSET = (1 << pin)
-#define GPIO_PORTB_CLRPIN(pin)	PORTB.OUTCLR = (1 << pin)
-#define GPIO_PORTB_SETOUT(pin)	PORTB.DIRSET = (1 << pin)
-#define GPIO_PORTB_SETIN(pin)	PORTB.DIRCLR = (1 << pin)
-#define GPIO_PORTB_READPIN(pin)	((PORTB.IN & (1 << pin)) != 0)
-#endif
-
 #define GPIO_SETOUTPUT(port, pin)		port.DIRSET = (1 << pin)
 #define GPIO_SETPIN(port, pin)			port.OUTSET = (1 << pin)
 #define GPIO_CLRPIN(port, pin)			port.OUTCLR = (1 << pin)
 #define GPIO_SETOUT(port, pin)			port.DIRSET = (1 << pin)
 #define GPIO_SETIN(port, pin)			port.DIRCLR = (1 << pin)
+#define GPIO_TOGGLEPIN(port, pin)		port.OUTTGL = (1 << pin)
 #define GPIO_READPIN(port, pin)			((port.IN & (1 << pin)) != 0)
 
 #define GPIO_GETPINCTRL(pin)		PIN ## pin ## CTRL

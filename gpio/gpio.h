ed88bf80d5cc160c3e0fd3e30ad624bf0bbc11e1
charlesgervais <charles.gervais.dumont@gmail.com>
2020-10-16 14:04:14 -0400

    GPIO: Use universal macros

--- gpio/gpio.h
+++ gpio/gpio.h
@@ -34,6 +34,21 @@ typedef enum {ePortA, ePortB, ePortC} ePort_t;
 #define GPIO_PORTB_READPIN(pin)	((PORTB.IN & (1 << pin)) != 0)
 #endif
 
+#define GPIO_SETOUTPUT(port, pin)		port.DIRSET = (1 << pin)
+#define GPIO_SETPIN(port, pin)			port.OUTSET = (1 << pin)
+#define GPIO_CLRPIN(port, pin)			port.OUTCLR = (1 << pin)
+#define GPIO_SETOUT(port, pin)			port.DIRSET = (1 << pin)
+#define GPIO_SETIN(port, pin)			port.DIRCLR = (1 << pin)
+#define GPIO_READPIN(port, pin)			((port.IN & (1 << pin)) != 0)
+
+#define GPIO_GETPINCTRL(pin)		PIN ## pin ## CTRL
+
+#define GPIO_SETUP_INPUT(port, pin, inverted, pullup, portConfig)	\
+do {	\
+	port.DIRCLR = (1 << pin);	\
+	port.GPIO_GETPINCTRL(pin) = ((inverted == pdTRUE) << PORT_INVEN_bp) | ((pullup == pdTRUE) << PORT_PULLUPEN_bp) | (portConfig & PORT_ISC_gm);	\
+} while (0)
+
 void vGpioSetupInput(const ePort_t port, const uint8_t pin, const BaseType_t inverted, BaseType_t pullup, const PORT_ISC_t portConfig);
 void vGpioSetupOutput(const ePort_t port, const uint8_t pin);
 

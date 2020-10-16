4ef040d9519a9db6d00aa3021a9446baf3100949
charlesgervais <charles.gervais.dumont@gmail.com>
2020-10-16 13:44:52 -0400

    UART: Update port definitions to registers

--- comms/uart.h
+++ comms/uart.h
@@ -14,13 +14,13 @@
 #define UART_BAUDRATE(BAUD_RATE) ((float)(CPU_FREQUENCY * 64 / (16 * (float)BAUD_RATE)) + 0.5)
 
 #ifdef TINYx14
-	#define UART_PORT_RX_ALT	GPIO_PORTA
+	#define UART_PORT_RX_ALT	PORTA
 	#define UART_PIN_RX_ALT		2
-	#define UART_PORT_TX_ALT	GPIO_PORTA
+	#define UART_PORT_TX_ALT	PORTA
 	#define UART_PIN_TX_ALT		1
-	#define UART_PORT_RX		GPIO_PORTB
+	#define UART_PORT_RX		PORTB
 	#define UART_PIN_RX			3
-	#define UART_PORT_TX		GPIO_PORTB
+	#define UART_PORT_TX		PORTB
 	#define UART_PIN_TX			2
 #else
 	#error UART: Unsupported MCU!

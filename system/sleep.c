30e30b4a90c8f3c0be090edc7fd07673cc303ba8
charlesgervais <charles.gervais.dumont@gmail.com>
2020-10-21 18:41:20 -0400

    Sleep: Flatten to macros

--- system/sleep.c
+++ system/sleep.c
@@ -6,35 +6,3 @@
  */ 
 
 #include "sleep.h"
-
-#include <avr/io.h>
-
-inline void vTinySleepIdle(void) {
-	SLPCTRL.CTRLA = SLPCTRL_SEN_bm | SLPCTRL_SMODE_IDLE_gc;
-	
-	#ifndef DEBUG
-		asm volatile (
-		"    sleep\n"
-		);
-	#endif
-}
-
-inline void vTinySleepStandBy(void) {
-	SLPCTRL.CTRLA = SLPCTRL_SEN_bm | SLPCTRL_SMODE_STDBY_gc;
-	
-	#ifndef DEBUG
-		asm volatile (
-		"    sleep\n"
-		);
-	#endif
-}
-
-inline void vTinySleepPowerDown(void) {
-	SLPCTRL.CTRLA = SLPCTRL_SEN_bm | SLPCTRL_SMODE_PDOWN_gc;
-	
-	#ifndef DEBUG
-		asm volatile (
-		"    sleep\n"
-		);
-	#endif
-}
--- system/sleep.h
+++ system/sleep.h
@@ -9,8 +9,24 @@
 #ifndef SLEEP_H_
 #define SLEEP_H_
 
-void vTinySleepIdle(void);
-void vTinySleepStandBy(void);
-void vTinySleepPowerDown(void);
+#include <avr/io.h>
+
+#define TINYSLEEP_IDLE()											\
+	do {															\
+		SLPCTRL.CTRLA = SLPCTRL_SEN_bm | SLPCTRL_SMODE_IDLE_gc;		\
+		asm volatile ("    sleep\n");								\
+	} while(0)
+
+#define TINYSLEEP_STANDBY()											\
+	do {															\
+		SLPCTRL.CTRLA = SLPCTRL_SEN_bm | SLPCTRL_SMODE_STDBY_gc;	\
+		asm volatile ("    sleep\n");								\
+	} while(0)
+
+#define TINYSLEEP_POWERDOWN()										\
+	do {															\
+		SLPCTRL.CTRLA = SLPCTRL_SEN_bm | SLPCTRL_SMODE_PDOWN_gc;	\
+		asm volatile ("    sleep\n");								\
+	} while(0)
 
 #endif /* SLEEP_H_ */

7d1f7905ed32afa434ca2ae7a762bedb7c6b8119
charlesgervais <charles.gervais.dumont@gmail.com>
2021-05-31 15:14:10 -0400

    Add check before enabling Watchdog

--- system/wdt.h
+++ system/wdt.h
@@ -12,14 +12,20 @@
 #include "ccp.h"
 #include <avr/io.h>
 
-#define TINYWATCHDOG_ENABLE(frequency)		ccp_write_io((void *)&(WDT.CTRLA), frequency | WDT_WINDOW_OFF_gc)
+#define TINYWATCHDOG_SETFREQUENCY(frequency)	ccp_write_io((void *)&(WDT.CTRLA), frequency | WDT_WINDOW_OFF_gc)
 
 #define TINYWATCHDOG_WAITFORSYNC()			while(WDT.STATUS & WDT_SYNCBUSY_bm) {}
 
-#define TINYWATCHDOG_DISABLE()	do {			\
-	TINYWATCHDOG_WAITFORSYNC()					\
-	TINYWATCHDOG_ENABLE(WDT_PERIOD_OFF_gc);		\
-	TINYWATCHDOG_WAITFORSYNC()					\
+#define TINYWATCHDOG_ENABLE(frequency)	do {		\
+	TINYWATCHDOG_WAITFORSYNC()						\
+	TINYWATCHDOG_SETFREQUENCY(WDT_PERIOD_OFF_gc);	\
+	TINYWATCHDOG_WAITFORSYNC()						\
+} while(0)
+
+#define TINYWATCHDOG_DISABLE()	do {				\
+	TINYWATCHDOG_WAITFORSYNC()						\
+	TINYWATCHDOG_SETFREQUENCY(WDT_PERIOD_OFF_gc);	\
+	TINYWATCHDOG_WAITFORSYNC()						\
 } while(0)
 
 #define TINYWATCHDOG_RESET()				asm volatile ("    wdr"	"\n")					
